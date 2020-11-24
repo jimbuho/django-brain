6212d37ec63c4538888b313dd7fb058b0bc51332
jimbuho <dgonzalez.jim@gmail.com>
2020-11-24 11:52:55 -0500

    Add new version and files for pip upload

--- brainutils/__init__.py
+++ brainutils/__init__.py
@@ -1,5 +1,5 @@
 """My app description"""
-VERSION = (0, 1, 2)
+VERSION = (0, 1, 3)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Diego Gonzalez (jimbuho)'
 __email__ = 'dgonzalez.jim@gmail.com'
